b800caa3452f26625a05a354828feb1147dca29b
Beulah Karrolla <beulah.karrolla@gmail.com>
2024-06-06 09:53:44 -0500

    Update script.js - removed debug functions

--- static/js/script.js
+++ static/js/script.js
@@ -46,61 +46,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 // Define variables to hold the recording stream and media recorder
-let audioStream;
-let mediaRecorder;
 
-// Function to start recording audio
-function startRecording() {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(stream => {
-            audioStream = stream;
-            mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.start();
-            console.log("Recording started...");
-        })
-        .catch(error => {
-            console.error("Error accessing microphone:", error);
-        });
-}
 function getCookie(name) {
     const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
     return cookieValue ? cookieValue.pop() : '';
 }
-// Function to stop recording audio and send it to the backend
-function stopRecording() {
-    if (mediaRecorder && mediaRecorder.state === "recording") {
-        mediaRecorder.stop();
-        console.log("Recording stopped...");
 
-        // Retrieve recorded audio data
-        mediaRecorder.ondataavailable = event => {
-            const audioBlob = event.data;
-            
-            // Convert audio blob to a file
-            const audioFile = new File([audioBlob], 'recording.wav', { type: audioBlob.type });
-            
-            // Send audio file to backend (replace 'upload-audio' with your backend endpoint)
-            const formData = new FormData();
-            formData.append('audio', audioFile);
-            const csrftoken = getCookie('csrftoken');
-            fetch('/process_audio/', {
-                method: 'POST',
-                headers: {
-                    'X-CSRFToken': csrftoken
-                },
-                body: JSON.stringify({ kwd: kwd.value })
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log("Received response from backend:", data);
-                // Handle response from backend (e.g., display results)
-            })
-            .catch(error => {
-                console.error("Error sending audio data to backend:", error);
-            });
-        };
-    }
-}
 
 // Event listener for the "Start Recording" button
 document.getElementById('stop-btn').addEventListener('click', function(){
@@ -108,58 +59,7 @@ document.getElementById('stop-btn').addEventListener('click', function(){
     var pageUrl = '/page/' + queryParams; 
     window.location.href = pageUrl;
 });
-document.getElementById('startbtn').addEventListener('click', function(){
-    const csrftoken = getCookie('csrftoken');
-    kwd = document.getElementById('key-word')
-    fetch('/process_audio/', {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': csrftoken
-        },
-        
-        body: JSON.stringify({ kwd: kwd.value, res:true  })
-    })
-    
-    .then(response => response.text())
-    .then(data => {
-        console.log("Received response from backend:", data);
-        //const responseContainer = document.getElementById('responseContainer');
-        //var queryParams = '?stream=value1&param2=value2';
-        //var pageUrl = '/page/' + queryParams; 
-        //window.location.href = pageUrl;
-        //responseContainer.innerHTML = data;
-        //$('#result-container').html(data);
-        // Handle response from backend (e.g., display results)
-    })
-    .catch(error => {
-        console.error("Error sending audio data to backend:", error);
-    });
-});
-document.getElementById('stopbtn').addEventListener('click', function(){
-    const csrftoken = getCookie('csrftoken');
-    kwd = document.getElementById('key-word')
-    fetch('/process_audio/', {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': csrftoken
-        },
-        
-        body: JSON.stringify({ kwd: kwd.value, res:false  })
-    })
-    
-    .then(response => response.text())
-    .then(data => {
-        console.log("Received response from backend:", data);
-        const testModeContent = document.getElementById('testModeContent');
-    
-        testModeContent.style.display = 'block';
-        document.body.innerHTML = data
-        })
-    .catch(error => {
-        console.error("Error in stop button:", error);
-    });
-    
-});
+
 
 // Event listener for the "Stop Recording" button
 
